7c040f822841f4408540774a8063d0db33c01235
denser <mian@denser.ru>
2025-01-04 20:47:02 -0300

    Шаг 1: Подготовка и базовая структура приложения

---
+++ dev_trusted_node/app/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from database import init_db
+
+app = Flask(__name__)
+
+# Инициализация базы данных при старте приложения
+with app.app_context():
+    init_db()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000) # порт можно поменять на нужный
---
+++ dev_trusted_node/app/schema.sql
@@ -0,0 +1,1 @@
+-- Create the users tablec
